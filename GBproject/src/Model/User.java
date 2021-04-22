b64b1cf2c6f4a6c1b587caf397d314d4d5586a95
bhagya-it19110608 <bhagyaweerasinghe02@gmail.com>
2021-04-22 11:56:29 +0530

    ususerCRUD completed

---
+++ GBproject/src/Model/User.java
@@ -0,0 +1,100 @@
+package Model;
+
+public class User {
+
+	int uId;
+	String firstName;
+	String lastName;
+	String email;
+	String gender;
+	String occupation;
+	String phone;
+	String username;
+	String password;
+		
+	public User() {
+	}
+
+	public User(int uId, String firstName, String lastName, String email, String gender, String occupation, String phone, String username, String password ) {
+		super();
+		this.uId = uId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.gender = gender;
+		this.occupation = occupation;
+		this.phone = phone;
+		this.username = username;
+		this.password = password;
+	}
+
+	public int getuId() {
+		return uId;
+	}
+
+	public void setuId(int uId) {
+		this.uId = uId;
+	}
+
+	public String getfirstName() {
+		return firstName;
+	}
+
+	public void setfirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getlastName() {
+		return lastName;
+	}
+
+	public void setlastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getemail() {
+		return email;
+	}
+
+	public void setemail(String email) {
+		this.email = email;
+	}
+	public String getgender() {
+		return gender;
+	}
+
+	public void setgender(String gender) {
+		this.gender = gender;
+	}
+	public String getoccupation() {
+		return occupation;
+	}
+
+	public void setoccupation(String occupation) {
+		this.occupation = occupation;
+	}
+	
+	public String getphone() {
+		return phone;
+	}
+
+	public void setphone(String phone) {
+		this.phone = phone;
+	}
+	
+	public String getusername() {
+		return username;
+	}
+
+	public void setusername(String username) {
+		this.username = username;
+	}
+	
+	public String getpassword() {
+		return password;
+	}
+
+	public void setpassword(String password) {
+		this.password = password;
+	}
+}
---
+++ GBproject/src/com/UserService.java
@@ -0,0 +1,110 @@
+package com;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import Controller.UserController;
+import Model.User;
+
+@Path("/user")
+public class UserService {
+
+	UserController us = new UserController();
+
+	
+	//get
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readUser() {
+		return us.viewUsers();
+	}
+
+
+	//add
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON) 
+	@Produces(MediaType.TEXT_PLAIN)
+	public String enterUsers(String TypeData) {
+		// Convert the input string to a JSON object
+		JsonObject djosnObj = new JsonParser().parse(TypeData).getAsJsonObject();
+	
+		User u = new User();
+		
+		u.setfirstName(djosnObj.get("firstName").getAsString());
+		u.setlastName(djosnObj.get("lastName").getAsString());
+		u.setemail(djosnObj.get("email").getAsString());
+		u.setgender(djosnObj.get("gender").getAsString());
+		u.setoccupation(djosnObj.get("occupation").getAsString());
+		u.setphone(djosnObj.get("phone").getAsString());
+		u.setusername(djosnObj.get("username").getAsString());
+		u.setpassword(djosnObj.get("password").getAsString());
+		
+		// Read the values from the JSON object
+		String output = us.addUser(u);
+		return output;
+
+	}
+
+	
+	
+	
+	//update
+	@PUT
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String updateUsers(String TypeData) {
+
+		// Convert the input string to a JSON object
+		JsonObject djosnObj = new JsonParser().parse(TypeData).getAsJsonObject();
+		User u = new User();
+
+		u.setuId(djosnObj.get("uId").getAsInt());
+		u.setfirstName(djosnObj.get("firstName").getAsString());
+		u.setlastName(djosnObj.get("lastName").getAsString());
+		u.setemail(djosnObj.get("email").getAsString());
+		u.setgender(djosnObj.get("gender").getAsString());
+		u.setoccupation(djosnObj.get("occupation").getAsString());
+		u.setphone(djosnObj.get("phone").getAsString());
+		u.setusername(djosnObj.get("username").getAsString());
+		u.setpassword(djosnObj.get("password").getAsString());
+		
+			
+		String output = us.updateUser(u);
+		return output;
+		
+	}
+	
+
+	//delete
+		
+	@DELETE
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deleteUsers(String TypeData) {
+		// Convert the input string to a JSON object
+		JsonObject doc = new JsonParser().parse(TypeData).getAsJsonObject();
+
+		User u = new User();
+			
+		// Read the value from the element <ID>
+	   	u.setuId(doc.get("uId").getAsInt());
+			
+		String output = us.deleteUser(u);
+		return output;
+		
+	}
+	
+}
